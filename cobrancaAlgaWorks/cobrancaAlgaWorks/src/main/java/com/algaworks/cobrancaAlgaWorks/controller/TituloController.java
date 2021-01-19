37fdf362e29554fbc82ae29e5762864002d2dbe7
MarciaMartins <marciapmartins@gmail.com>
2021-01-19 16:42:08 +0000

    mantendo os valores do formulário

--- cobrancaAlgaWorks/cobrancaAlgaWorks/src/main/java/com/algaworks/cobrancaAlgaWorks/controller/TituloController.java
+++ cobrancaAlgaWorks/cobrancaAlgaWorks/src/main/java/com/algaworks/cobrancaAlgaWorks/controller/TituloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.cobrancaAlgaWorks.model.StatusTitulo;
 import com.algaworks.cobrancaAlgaWorks.model.Titulo;
@@ -31,18 +32,19 @@ public class TituloController {
 	}
 	
 	@RequestMapping( method =RequestMethod.POST )
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		
 		if(errors.hasErrors()) {
 			System.out.println(">>Erro salvar");
-			return mv;
+			return "CadastroTitulo";
 		}
 				
 		System.out.println(">>Banco<<");
 		titulos.save(titulo);
-		mv.addObject("mensagem", "Título salvo com sucesso!");
+		
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
 		System.out.println(">>"+ titulo.getDataVencimento());
-		return mv;
+		return "redirect:/titulos/novo";
 	}
 	
 	@RequestMapping
